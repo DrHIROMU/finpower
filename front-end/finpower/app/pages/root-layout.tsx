ce78fa5035d542df6e0c70291511f4ba237442d6
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2025-01-17 13:05:22 +0800

    [add][front-end] Add route layout

--- front-end/finpower/app/pages/root-layout.tsx
+++ front-end/finpower/app/pages/root-layout.tsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router"
+
 export default function RootLayout(){
   return <>
   <header>Header</header>
--- front-end/finpower/app/routes.ts
+++ front-end/finpower/app/routes.ts
@@ -1,5 +1,8 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
-export default [index("pages/login.tsx"),
-  route("login", "pages/login.tsx")
+export default [
+  route("login", "pages/login.tsx"),
+  layout("pages/root-layout.tsx", [ 
+    route("home", "pages/home.tsx"),
+  ]),
 ] satisfies RouteConfig;
