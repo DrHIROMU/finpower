5436b5fe99eb8e73d54830cf02791bcd6b76fe07
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2025-01-22 15:43:17 +0800

    [modify][front-end] Change front-end port

--- front-end/finpower/vite.config.ts
+++ front-end/finpower/vite.config.ts
@@ -5,6 +5,12 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: 4200
+  },
+  preview: {
+    port: 8000
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
