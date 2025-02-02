6836b88a7aa0df08975f841e6ed9e278c7b38e24
Lin Kung Hung <drhiromu@gmail.com>
2025-02-02 21:58:00 +0800

    [modify][back-end] Create login API

--- back-end/finpower/src/main/java/khlin/finpower/auth/service/AuthService.java
+++ back-end/finpower/src/main/java/khlin/finpower/auth/service/AuthService.java
@@ -2,9 +2,12 @@ package khlin.finpower.auth.service;
 
 import khlin.finpower.auth.dto.LoginRequest;
 import khlin.finpower.auth.util.JWTUtils;
+import khlin.finpower.user.entity.User;
 import khlin.finpower.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -12,6 +15,12 @@ public class AuthService {
     UserRepository userRepository;
 
     public String login(LoginRequest loginRequest){
+        Optional<User> loginUser = userRepository.findByEmailAndPassword(loginRequest.getEmail(), loginRequest.getPassword());
+
+        if(loginUser.isEmpty()){
+            return null;
+        }
+
         return JWTUtils.generateToken(loginRequest.getEmail());
     }
 }
