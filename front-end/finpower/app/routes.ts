f4091b0fa4dd56cff44ae93214cb409632a36c80
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2025-02-11 10:35:17 +0800

    [modify][front-end] Modify users function route settings

--- front-end/finpower/app/routes.ts
+++ front-end/finpower/app/routes.ts
@@ -9,7 +9,13 @@ import {
 export default [
   layout("pages/root-layout.tsx", [
     index("pages/home.tsx"),
-    route("users-management", "pages/user/users-management.tsx"),
+    ...prefix("users", [
+      index("pages/user/users-management.tsx"),
+      ...prefix(":userId", [
+        index("pages/user/user-detail.tsx"),
+        route("edit", "pages/user/edit-user.tsx"),
+      ]),
+    ]),
   ]),
   route("logout", "pages/logout.tsx"),
   route("login", "pages/login.tsx"),
