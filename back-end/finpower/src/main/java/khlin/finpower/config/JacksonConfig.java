80afcad7167c7737edc261bb3c311904dcb55bef
Lin Kung Hung <drhiromu@gmail.com>
2025-02-23 23:07:17 +0800

    [fix][back-end] fix Jackson mapper error caused by Date Instant.

--- back-end/finpower/src/main/java/khlin/finpower/config/JacksonConfig.java
+++ back-end/finpower/src/main/java/khlin/finpower/config/JacksonConfig.java
@@ -1,19 +1,19 @@
 package khlin.finpower.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import khlin.finpower.util.api.StringTrimToNullDeserializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(String.class, new StringTrimToNullDeserializer());
-        mapper.registerModule(module);
-        return mapper;
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return builder -> {
+            SimpleModule module = new SimpleModule();
+            module.addDeserializer(String.class, new StringTrimToNullDeserializer());
+            builder.modulesToInstall(module);
+        };
     }
 }
